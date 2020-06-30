37024b34c3a76683c4b666e35f86e50dc26ae095
jinwoo park <swat018@gmail.com>
2020-07-01 04:30:16 +0900

    스프링 데이터 Common: QueryDSL

--- src/main/java/com/swat018/querydsldemo/account/AccountRepository.java
+++ src/main/java/com/swat018/querydsldemo/account/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.swat018.querydsldemo.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account> {
+
 }
--- src/test/java/com/swat018/querydsldemo/account/AccountRepositoryTest.java
+++ src/test/java/com/swat018/querydsldemo/account/AccountRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.swat018.querydsldemo.account;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +20,12 @@ public class AccountRepositoryTest {
 
     @Test
     public void crud() {
+        QAccount account = QAccount.account;
+        Predicate predicate = account
+                .firstname.containsIgnoreCase("jinwoo")
+                .and(account.lastname.startsWith("park"));
 
+        Optional<Account> one = accountRepository.findOne(predicate);
+        assertThat(one).isEmpty();
     }
 }
